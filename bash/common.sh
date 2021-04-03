6f119b47721723183e71def2983a9edcce53ea3e
kumanobori <jirosv01@gmail.com>
2021-04-03 20:43:27 +0900

    バッチテンプレート改修

--- bash/common.sh
+++ bash/common.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+# --------------------------------------------------------
+# - 概要：バッチ開始日時文字列の生成と、ログ用関数の定義。
+# - 前提：各関数を呼び出すまでに、呼び出し元で変数${LOGPATH}が指定されること
+# --------------------------------------------------------
 
 # 日付変数
 START_TIMESTAMP="$(date "+%Y%m%d%H%M%S")"
-START_YMDHMS="${START_TIMESTAMP:0:8} ${START_YMDHMS:9:6}"
-START_YMDHM="${START_TIMESTAMP:0:12}"
+START_YMD_HMS="${START_TIMESTAMP:0:8}_${START_TIMESTAMP:8:6}"
+START_YMD_HM="${START_TIMESTAMP:0:8}_${START_TIMESTAMP:8:4}"
 START_YMD="${START_TIMESTAMP:0:8}"
 
-# ログ出力用変数
-LOGDIR="$(pwd)/log"
-LOGFILE="${START_YMDHM}.log"
-LOGPATH="${LOGDIR}/${LOGFILE}"
+# ログレベル
+LOG_LEVEL=0
+LOG_LEVEL_DEBUG=1
+LOG_LEVEL_INFO=2
+LOG_LEVEL_WARN=3
+LOG_LEVEL_ERROR=4
+LOG_LEVEL_NONE=5
 
 # ログ出力関数
 function log() {
@@ -19,15 +26,45 @@ function log() {
 	echo "${LOG}"
 }
 function logError() {
-	echo "$(log "[ERROR]" "$1")"
+	if [ "${LOG_LEVEL}" -le "${LOG_LEVEL_ERROR}" ]; then
+		echo "$(log "[ERROR]" "$1")"
+	fi
 }
 function logWarn() {
-	echo "$(log "[WARN] " "$1")"
+	if [ "${LOG_LEVEL}" -le "${LOG_LEVEL_WARN}" ]; then
+		echo "$(log "[WARN] " "$1")"
+	fi
 }
 function logInfo() {
-	echo "$(log "[INFO] " "$1")"
+	if [ "${LOG_LEVEL}" -le "${LOG_LEVEL_INFO}" ]; then
+		echo "$(log "[INFO] " "$1")"
+	fi
 }
 function logDebug() {
-	echo "$(log "[DEBUG]" "$1")"
+	if [ "${LOG_LEVEL}" -le "${LOG_LEVEL_DEBUG}" ]; then
+		echo "$(log "[DEBUG]" "$1")"
+	fi
+}
+
+# コマンドをログ出力＋実行する関数
+# 実行結果はログレベルにかかわらず出力する。
+function doEval() {
+	eval $1 >> ${LOGPATH} 2>&1
+}
+function evalError() {
+	logError "command: $1"
+	doEval "$1"
+}
+function evalWarn() {
+	logWarn "command: $1"
+	doEval "$1"
+}
+function evalInfo() {
+	logInfo "command: $1"
+	doEval "$1"
+}
+function evalDebug() {
+	logDebug "command: $1"
+	doEval "$1"
 }
 
--- bash/main.sh
+++ bash/main.sh
@@ -1,32 +1,79 @@
 #!/bin/bash
-# 
-# スクリプト概要：
-# 
-# スクリプト詳細：
-# 
-# 前提： 
-# 
-# 引数：
-# 
 
-cd `dirname $0`
+function help() {
+	echo '概要：'
+	echo '詳細：'
+	echo '前提：'
+	echo '引数：'
+}
+
+
+
+function init() {
+	logInfo 'init start.'
+	
+	logError "$START_TIMESTAMP"
+	logWarn "$START_YMD_HMS"
+	logInfo "$START_YMD_HM"
+	logDebug "$START_YMD"
+	
+	logInfo 'init term.'
+	return 0
+}
+
+function main() {
+	logInfo 'main start.'
+	
+	cat "${SCRIPT_DIR}/input.txt" | while read line
+	do
+		IFS=','
+		items=(${line})
+		unset IFS
+		logDebug '-------------'
+		# logDebug ${line}
+		# logDebug ${items[0]}
+		# logDebug ${items[1]}
+		# logDebug ${items[2]}
+		detail ${items[0]} ${items[1]} ${items[2]}
+	done
+	
+	
+	logInfo 'main term.'
+	return 0
+}
+
+function detail() {
+	ARG1="$1"
+	ARG2="$2"
+	ARG3="$3"
+	logDebug "detail: ${ARG1} ${ARG2} ${ARG3}"
+}
+
+# -----------------------------------
+# ----- バッチ共通前処理 START -----
+# -----------------------------------
 . ./common.sh
 
-logInfo 'start.'
+# ログレベル設定
+LOG_LEVEL=${LOG_LEVEL_DEBUG}
+# ログファイル設定
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+LOG_DIR=${SCRIPT_DIR}/log
+mkdir -p ${LOG_DIR}
+LOG_FILE="${START_YMD_HMS}.log"
+LOGPATH=${LOG_DIR}/${LOG_FILE}
+logInfo "LOGPATH=${LOGPATH}"
 
-# 引数個数チェック
+# 引数チェック
 if [ $# -ne 0 ]; then
 	echo '引数は0個でなければいけません。'
+	help
 	exit 1
 fi
 
-function init() {
-	logError "$START_TIMESTAMP"
-	logError "$START_YMDHMS"
-	logWarn "$START_YMDHM"
-	logDebug "$START_YMD"
-	return 0
-}
+
+# 処理開始
+logInfo 'start.'
 
 init
 INIT_RESULT=$?
@@ -34,5 +81,17 @@ if [ ${INIT_RESULT} -ne 0 ]; then
 	logError "init failed. error code = ${INIT_RESULT}"
 fi
 
+main
+MAIN_RESULT=$?
+if [ ${MAIN_RESULT} -ne 0 ]; then
+	logError "main failed. error code = ${MAIN_RESULT}"
+fi
+
 logInfo 'term.'
 exit 0
+
+# -----------------------------------
+# ----- バッチ共通前処理 END -----
+# -----------------------------------
+
+

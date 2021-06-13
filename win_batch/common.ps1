690bc46f7cb738ef52bd9ef3dd6c17453009bb77
kumanobori <jirosv01@gmail.com>
2021-06-13 18:15:08 +0900

    ログ関数追加

---
+++ win_batch/common.ps1
@@ -0,0 +1,60 @@
+﻿# 開始日時取得
+$startDate = Get-Date
+$startYmdHms = $startDate.ToString("yyyyMMddHHmmss")
+
+# $LOGPATH = $PSScriptRoot+ "\" + $Args[0] + "_" + $startYmdHms + ".log"
+$LOGPATH = "c:\data\log.txt"
+
+# ログファイルモード
+$LOG_FILE_MODE = $true
+
+# バッチに適用するログレベル
+$LOG_LEVEL = 1
+
+# ログレベル定数
+$LOG_LEVEL_TRACE = 1
+$LOG_LEVEL_DEBUG = 2
+$LOG_LEVEL_INFO = 3
+$LOG_LEVEL_WARN = 4
+$LOG_LEVEL_ERROR = 5
+$LOG_LEVEL_NONE = 6
+
+# ログレベルに対応する動作
+$VerbosePreference = 'Continue'
+$DebugPreference = 'Continue'
+$InformationPreference = 'Continue'
+$WarningPreference = 'Continue'
+$ErrorActionPreference = 'Continue'
+
+# ログ出力関数
+function log([int] $level, [string] $funcName, [string] $prefix, [string] $message) {
+    if($level -lt $LOG_LEVEL) {
+        return
+    }
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss "
+    
+    if ($LOG_FILE_MODE) {
+        echo ("{0}{1}{2}" -f $timestamp, $prefix, $message) >> $LOGPATH
+    } else {
+        & $funcName ("{0}{1}{2}" -f $timestamp, $prefix, $message)
+    }
+}
+
+function logError([string] $message) {
+    log $LOG_LEVEL_ERROR "Write-Error" "[ERROR] " $message
+}
+function logWarn([string] $message) {
+    log $LOG_LEVEL_WARN "Write-Warning" "[WARN] " $message
+}
+function logInfo([string] $message) {
+    # log $LOG_LEVEL_INFO "Write-Information" "[INFO] " $message
+    log $LOG_LEVEL_INFO "Write-Host" "[INFO] " $message
+}
+function logDebug([string] $message) {
+    # log $LOG_LEVEL_DEBUG "Write-Debug" "[DEBUG] " $message
+    log $LOG_LEVEL_DEBUG "Write-Host" "[DEBUG] " $message
+}
+function logTrace([string] $message) {
+    # log $LOG_LEVEL_TRACE "Write-Verbose" "[TRACE]" $message
+    log $LOG_LEVEL_TRACE "Write-Host" "[TRACE] " $message
+}
--- win_batch/main.ps1
+++ win_batch/main.ps1
@@ -1,11 +1,40 @@
-﻿# 管理者権限でない場合、現在のスクリプトを管理者権限で実行する
+﻿# カレントディレクトリをスクリプトのあるディレクトリに変更する
+# （スクリプトがそれを前提としていないなら不要）
+Set-Location -Path $PSScriptRoot
+. .\common.ps1 "log"
+
+logDebug "start"
+
+# 管理者権限でない場合、現在のスクリプトを管理者権限で実行して自分は終了
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
-    echo "not administrator"
-    Start-Process powershell.exe "-File `"$MyInvocation.MyCommand.path`"" -Verb RunAs $Args
+    $commandPath = $PSCommandPath
+    logDebug ("not admin. restart as admin:" + $commandPath)
+    
+    # 引数を、管理者権限実行用に作り直す
+    # 引数が、クォートされた空白を含む文字列である場合を想定。
+    # そこまで考えないでいいなら、Start-Process の $argsToAdminProcess の代わりに$Argsを入れても問題ない。
+    $argsToAdminProcess = ""
+    $Args.ForEach{
+        logDebug ("not admin: args: " + $PSItem)
+        $argsToAdminProcess += " `"$PSItem`""
+    }
+
+    # 実行
+    Start-Process powershell.exe "-File `"$commandPath`" $argsToAdminProcess"  -Verb RunAs
+    # デバッグ用。Pauseしないといけない理由は特にない。
+    Pause
     exit
 }
 
 
-echo "administrator"
-pause
+
+logDebug "admin"
+
+# 管理権限実行に渡ってきた引数を確認する
+$Args.ForEach{
+    logDebug ("admin: args: " + $PSItem)
+}
+
+# デバッグ用。Pauseしないといけない理由は特にない。
+Pause
